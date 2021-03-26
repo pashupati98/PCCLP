d58019c8ecfb897ed0dab957c45108d3dc1e13b6
Pashupati Gupta <43728988+pashupati98@users.noreply.github.com>
2021-03-26 12:56:16 +0530

    Update settings.py

--- webapp/vst/settings.py
+++ webapp/vst/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xwaw00^ja2x=fe1(6meu72hrfc_!x1&d*-fuxz-cxo9yb9mv8b'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
